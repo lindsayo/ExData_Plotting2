f09f4008554737eb5759134f0c7ae1955717f264
lindsayo <lindsayo@240280.no-reply.drupal.org>
2015-09-21 22:18:24 -0600

    Completed plot1.PNG

--- plot1.R
+++ plot1.R
@@ -6,12 +6,23 @@ SCC <- readRDS("Source_Classification_Code.rds")
 library(dplyr)
 
 ## Subset NEI by each of the years
-years <- c(1999,2002,2005,2008)
+NEI99 <- filter(NEI, year == '1999')
+NEI02 <- filter(NEI, year == '2002')
+NEI05 <- filter(NEI, year == '2005')
+NEI08 <- filter(NEI, year == '2008')
+
+## Sum the Emissions values for each year
 sum99 <- sum(NEI99$Emissions)
 sum02 <- sum(NEI02$Emissions)
 sum05 <- sum(NEI05$Emissions)
 sum08 <- sum(NEI08$Emissions)
+
+## Set up the bar plot axes
+years <- c(1999,2002,2005,2008)
 sums <- c(sum99, sum02, sum05, sum08)
 
+## Plot it!
+png(file="plot1.PNG", bg="transparent")
 barplot(sums, names.arg=years)
 text(plotIt, sums, labels=round(sums, digits=0), pos=1, offset=.3)
+dev.off()
